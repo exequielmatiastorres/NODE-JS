f5ec6c7eadd8e7f283404bf3289a85ba1c47e56b
exequielmatiastorres <exequieltorres0109@gmail.com>
2023-01-21 19:37:36 -0300

    creando proyecto general node

---
+++ indexNodejs.js
@@ -0,0 +1,22 @@
+
+
+
+/*
+¿Que es node js? no es un lenguaje de programación, tampoco es un framework
+entorno de ejecucion de javascript orientado a eventos asincronicos
+
+Utiliza un enfoque de eventos asincronos; lo que significa que puede realizar multiples tareas asincronas tales como leer, escribir en el servidor, conectarse con una base de datos o enviar request de formulario
+
+ventajas: escribes codigo de javascript (ES6+)
+NPM con una gran cantidad de paquetes
+Apps Monolíticas, Micro servidores o API'S
+
+¿que es express?
+Es un framework para node.js, muy minimalista y con una serie de opciones que son comunes para crear sitios web y aplicaciones moviles.
+Su Principal ventaja es que puedes instalar los paquetes que vas
+necesitando en tu aplicación y no tiene una gran cantidad de
+herramientas ya incluidas
+
+
+
+*/ 
